11cf72ade0109be182d48e712c2fee5ed47b7141
kyokuro7 <baongrhava@gmail.com>
2025-09-22 05:45:19 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,6 +1,5 @@
 {
   "tokens": [
-    "8309037755:AAHM78iKbzYSrSmyGm1L8jBPbOIqgOlNKZs"
-    "8278004761:AAHBsLQeVUtTxh-8bfdG3hBhZ0aui7hogbc"
+    "8309037755:AAHM78iKbzYSrSmyGm1L8jBPbOIqgOlNKZs"    
   ]
 }
